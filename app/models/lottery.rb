6c97252d0be7aa4d4b6703dda147a67f83d2a7da
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-14 01:11:54 +0900

    Update lottery.rb

--- app/models/lottery.rb
+++ app/models/lottery.rb
@@ -7,14 +7,6 @@ class Lottery < ActiveRecord::Base
   enum status: { running: 0, finished: 1, cancelled: 2 }
   enum draw_type: { by_time: 1, by_reply: 2 }
 
-  validates :topic_id, presence: true, uniqueness: true
-  validates :post_id, presence: true
-  validates :created_by_id, presence: true
-  validates :name, presence: true, length: { maximum: 255 }
-  validates :prize, presence: true
-  validates :winner_count, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :draw_type, presence: true
-
   def participating_user_count
     Post.where(topic_id: self.topic_id)
         .where("post_number > 1")
