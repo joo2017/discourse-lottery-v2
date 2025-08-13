31715eda67be32b659e29ca4c678c080e46b81c8
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-14 02:18:05 +0900

    Update check_lotteries.rb

--- jobs/scheduled/check_lotteries.rb
+++ jobs/scheduled/check_lotteries.rb
@@ -3,9 +3,11 @@ module Jobs
     every 1.minute
 
     def execute(args)
-      Lottery.running.find_each do |lottery|
-        if (lottery.by_time? && Time.now >= lottery.draw_at) ||
-           (lottery.by_reply? && lottery.topic.posts_count - 1 >= lottery.draw_reply_count)
+      # 使用常量来查询所有“进行中”的抽奖
+      Lottery.where(status: Lottery::STATUSES[:running]).find_each do |lottery|
+        # 使用常量进行比较
+        if (lottery.draw_type == Lottery::DRAW_TYPES[:by_time] && Time.now >= lottery.draw_at) ||
+           (lottery.draw_type == Lottery::DRAW_TYPES[:by_reply] && lottery.topic.posts_count - 1 >= lottery.draw_reply_count)
           
           LotteryManager.new(lottery).perform_draw
         end
