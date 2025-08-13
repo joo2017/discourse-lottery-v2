ed1f1259d8f9db6a8612c92823e3e54443971583
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-14 01:10:52 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,2 @@
-LotteryV2::Engine.routes.draw do
-  # This file is intentionally left blank if no public routes are needed.
-end
-
-Discourse::Application.routes.draw do
-  mount ::LotteryV2::Engine, at: "/lottery-v2"
-  get '/admin/plugins/lottery-v2' => 'admin/lottery_admin#index', constraints: StaffConstraint.new
-  put '/admin/plugins/lottery-v2/settings' => 'admin/lottery_admin#update_settings', constraints: StaffConstraint.new
-end
+# This file is intentionally left blank.
+# Routes are defined in plugin.rb for simplicity in this version.
